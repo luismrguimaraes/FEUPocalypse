1a5d6d3ff7b7322f3d90ae78c7ec481f1457c4d8
luismrguimaraes <guimaraesluis0@gmail.com>
2023-03-07 16:24:27 +0000

    Explosion needs some tweaks but the rest seems alright

--- Assets/BombExplosionScript.cs
+++ Assets/BombExplosionScript.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class BombExplosionScript : MonoBehaviour
 {
     public float damage = 50;
+    public Rigidbody2D rb;
+
+    private bool isFlameActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject mainChar = GameObject.FindGameObjectWithTag("Player");
+        Physics2D.IgnoreCollision(mainChar.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+
+        Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Player Projectiles and Attacks"));
     }
 
     // Update is called once per frame
@@ -18,10 +24,22 @@ public class BombExplosionScript : MonoBehaviour
         
     }
 
+    void SetFlameActive()
+    {
+        rb.simulated = true;
+        isFlameActive = true;
+    }
+
+    void SetFlameInactive()
+    {
+        rb.simulated = false;
+        isFlameActive = true;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Collider2D enemyHit = collision.collider;
-        if (enemyHit.gameObject.CompareTag("Enemy"))
+        if (enemyHit.gameObject.CompareTag("Enemy") && isFlameActive)
         {
             // Enemy hit
             enemyHit.GetComponent<EnemyScript>().Damage(damage);
