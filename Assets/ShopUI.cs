32fa19670186c52c238c18459b305293f8764ef1
Fab <fabiomiguelchenhuang@gmail.com>
2023-03-08 18:17:09 +0000

    added disable functionality to already bought items

--- Assets/ShopUI.cs
+++ Assets/ShopUI.cs
@@ -8,14 +8,14 @@ using UnityEngine.Windows;
 
 public class ShopUI : MonoBehaviour
 {
-    enum ItemType { FlameBreath, Molotov};
+    enum ItemType { FlameBreath, CocktailMolotov};
     private Transform container;
     private Transform shopItemTemplate;
     public GameObject pointer;
     private bool activeShop = false;
     private ItemType selectedType = ItemType.FlameBreath;
     private int itemsSize;
-
+    private List<ItemType> itemsBought = new List<ItemType>();
 
     // Prices of the Items
     public int flameBreathPrice = 100;
@@ -38,6 +38,11 @@ public class ShopUI : MonoBehaviour
 
         for (int i = 0; i < container.childCount; i++)
         {
+            if (checkIfItemBought(container.GetChild(i).name))
+            {
+                continue;
+            }
+
             TextMeshProUGUI itemPriceText = container.GetChild(i).Find("Canvas").Find("ItemPrice").GetComponent<TextMeshProUGUI>();
             int itemCost = Int32.Parse(itemPriceText.text);
             if (itemCost > availbleMoney)
@@ -49,6 +54,39 @@ public class ShopUI : MonoBehaviour
             itemPriceText.color = new Color32(241, 219, 0, 255);
         }
 
+
+    }
+
+    private void UpdateBoughtItemsCanvas(ItemType boughtItemType)
+    {
+        //LogicScript logicScript = GameObject.FindGameObjectWithTag("LogicManager").GetComponent<LogicScript>();
+
+        for (int i = 0; i < container.childCount; i++)
+        {
+            if(container.GetChild(i).name == boughtItemType.ToString())
+            {   
+                Image itemBackgound = container.GetChild(i).Find("Canvas").Find("ItemBackground").GetComponent<Image>();
+                itemBackgound.color = new Color32((byte)(itemBackgound.color.r*255.0f), (byte)(itemBackgound.color.g * 255.0f), (byte)(itemBackgound.color.b * 255.0f), 40);
+
+                Debug.Log(itemBackgound.color.a);
+
+                TextMeshProUGUI itemPriceText = container.GetChild(i).Find("Canvas").Find("ItemPrice").GetComponent<TextMeshProUGUI>();
+                itemPriceText.color = new Color32((byte)(itemPriceText.color.r * 255.0f), (byte)(itemPriceText.color.g * 255.0f), (byte)(itemPriceText.color.b * 255.0f), 40);
+
+                TextMeshProUGUI itemNameText = container.GetChild(i).Find("Canvas").Find("ItemName").GetComponent<TextMeshProUGUI>();
+                itemNameText.color = new Color32((byte)(itemNameText.color.r * 255.0f), (byte)(itemNameText.color.g * 255.0), (byte)(itemNameText.color.b*255.0f), 40);
+
+                Image CoinsImg = container.GetChild(i).Find("Canvas").Find("CoinsImg").GetComponent<Image>();
+                CoinsImg.color = new Color32((byte)(CoinsImg.color.r * 255.0f), (byte)(CoinsImg.color.g*255.0), (byte)(CoinsImg.color.b*255.0), 40);
+
+                Image ItemImg = container.GetChild(i).Find("Canvas").Find("ItemImg").GetComponent<Image>();
+                ItemImg.color = new Color32((byte)(ItemImg.color.r * 255.0f), (byte)(ItemImg.color.g * 255.0), (byte)(ItemImg.color.b * 255), 40);
+
+                return;
+            }
+
+        }
+        return;
 
     }
 
@@ -95,27 +133,81 @@ public class ShopUI : MonoBehaviour
             }
             if (UnityEngine.Input.GetKeyDown(KeyCode.B) )
             {
-                Debug.Log("Buying " + selectedType);
                 BuyItem();
             }
         }
+    }
+
+    private bool checkIfItemBought(ItemType itemType)
+    {
+        Debug.Log(itemsBought);
+        if(itemsBought.Count == 0)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < itemsBought.Count; i++)
+        {
+            if (itemsBought[i] == itemType)
+            {
+                return true; 
+            }
+        }
+
+        return false;
+    }
+
+    private bool checkIfItemBought(String itemType)
+    {
+        Debug.Log(itemsBought);
+        if (itemsBought.Count == 0)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < itemsBought.Count; i++)
+        {
+            if (itemsBought[i].ToString() == itemType)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void BuyItem()
     {
         GameObject logicManager = GameObject.FindGameObjectWithTag("LogicManager");
 
+        bool success = false;
+
         switch (selectedType)
         {
             case ItemType.FlameBreath:
-                logicManager.GetComponent<LogicScript>().BuyWeapon(flameBreathPrice, (int)selectedType);
-                return;
+                if (checkIfItemBought(selectedType))
+                {
+                    break;
+                }
+                success = logicManager.GetComponent<LogicScript>().BuyWeapon(flameBreathPrice, (int)selectedType);
+                break;
 
-            case ItemType.Molotov:
-                logicManager.GetComponent<LogicScript>().BuyWeapon(molotovPrice, (int)selectedType);
-                return;
+            case ItemType.CocktailMolotov:
+                if (checkIfItemBought(selectedType))
+                {
+                    break;
+                }
+                success = logicManager.GetComponent<LogicScript>().BuyWeapon(molotovPrice, (int)selectedType);
+                break;
             default:
-                return;
+                break;
+        }
+
+        if (success)
+        {
+            Debug.Log("Item bought = " + selectedType);
+            UpdateBoughtItemsCanvas(selectedType);
+            itemsBought.Add(selectedType);
         }
     }
 }
